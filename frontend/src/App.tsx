49cc42ac962072cd218a913e8bd6ee5c2ad25844
Marco Antonio Caldeira <caldeiran391@gmail.com>
2022-12-28 11:59:56 -0300

    Adicionando Front-End

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect, useState, useSyncExternalStore } from "react";
+import api from './services/api';
 
 function App() {
+
+  useEffect(() => {
+    api.get('/users').then(response => {
+      setUsers(response.data);
+    })
+  }, []);
+
   return (
     <div className="App">
-
+      {users.map(user => )}
     </div>
   );
 }
